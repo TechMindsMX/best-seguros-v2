4182ccebbf2db41434ac89e269f771c40df48fb2
egjimenezg <egjimenezg@gmail.com>
2015-11-20 15:41:25 -0600

    coding #2 creando log diariamiente

--- grails-app/conf/logback.groovy
+++ grails-app/conf/logback.groovy
@@ -1,11 +1,22 @@
 import grails.util.BuildSettings
 import grails.util.Environment
 
+def bySecond = timestamp("dd-MM-yyyy HH:mm:ss")
+
 // See http://logback.qos.ch/manual/groovy.html for details on configuration
 appender('STDOUT', ConsoleAppender) {
     encoder(PatternLayoutEncoder) {
         pattern = "%level %logger - %msg%n"
     }
+}
+
+appender('ROLLING',RollingFileAppender){
+  encoder(PatternLayoutEncoder){
+    pattern = "%level $logger - \t%m%n"
+  }
+  rollingPolicy(TimeBasedRollingPolicy){
+    FileNamePattern = "build/logs/best_seguros-%d{yyyy-MM}.log"
+  }
 }
 
 root(ERROR, ['STDOUT'])
