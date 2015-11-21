a18ee976182b7f57d4f6a3f88428eb12a40893f9
egjimenezg <egjimenezg@gmail.com>
2015-11-20 20:40:30 -0600

    coding #3 agregando clase para la información de la aseguradora

---
+++ grails-app/domain/com/bestseguros/Insurance.groovy
@@ -0,0 +1,22 @@
+package com.bestseguros
+
+class Insurance {
+
+  String code
+  String name
+  String country
+  String fiscalAddress
+  String phone
+  String contact
+  String rfc
+
+  static constraints = {
+    code blank:false,nullable:false
+    name blank:false,size:0..150,nullable:false
+    country blank:false,size:0..150
+    fiscalAddress blank:false,size:0..150
+    phone blank:false,size:0..20
+    rfc unique:true,blank:false,size:10..50
+  }
+
+}
