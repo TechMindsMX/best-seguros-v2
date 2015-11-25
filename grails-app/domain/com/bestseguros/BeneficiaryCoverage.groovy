4f85daa30eef5faa9ece1538bf4ee1f2c20c9954
Gamaliel Jimenez García <egjimenezg@gmail.com>
2015-11-25 17:08:21 -0600

    coding #6 agregando clase de dominio para definir los rangos de edades de los beneficiarios

---
+++ grails-app/domain/com/bestseguros/BeneficiaryCoverage.groovy
@@ -0,0 +1,14 @@
+package com.bestseguros
+
+class BeneficiaryCoverage{
+  BeneficiaryType beneficiaryType
+  Integer minAge
+  Integer maxAge
+  Integer renewal
+
+  static constraints = {
+    minAge min:0
+    minAge min:0
+  }
+
+}
