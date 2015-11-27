1d6ffc8ee91148ff3c9864b22efb44c080758626
Gamaliel Jimenez García <egjimenezg@gmail.com>
2015-11-27 09:57:05 -0600

    coding #8 quitando propiedades innecesarias de la clase de dominio sponsor

--- grails-app/domain/com/bestseguros/Sponsor.groovy
+++ grails-app/domain/com/bestseguros/Sponsor.groovy
@@ -2,21 +2,10 @@ package com.bestseguros
 
 class Sponsor {
 
-  String code
   String name
-  String country
-  String fiscalAddress
-  String phone
-  String contact
-  String rfc
 
   static constraints = {
-    code blank:false,nullable:false
     name blank:false,size:0..150,nullable:false
-    country blank:false,size:0..150
-    fiscalAddress blank:false,size:0..150
-    phone blank:false,size:0..20
-    rfc unique:true,blank:false
   }
 
 }
