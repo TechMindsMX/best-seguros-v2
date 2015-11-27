48f386b7eed76a98e82dfc5ebd831c6280ee751c
Gamaliel Jimenez García <egjimenezg@gmail.com>
2015-11-27 09:54:19 -0600

    coding #8 quitando el código de la clase de dominio cobertura

--- grails-app/domain/com/bestseguros/Coverage.groovy
+++ grails-app/domain/com/bestseguros/Coverage.groovy
@@ -1,13 +1,11 @@
 package com.bestseguros
 
 class Coverage {
-  
-  String code
+
   String name
 
   static constraints = {
-    code blank:false,nullable:true 
-    name blank:false,nullable:true 
+    name blank:false,nullable:true
   }
 
 }
