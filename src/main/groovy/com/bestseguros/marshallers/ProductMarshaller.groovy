cfecf20bf7ab83a87698cf49451087fcd7752cd6
egjimenezg <egjimenezg@gmail.com>
2015-12-01 11:45:17 -0600

    coding #8 actualizando marshaller del producto

--- src/main/groovy/com/bestseguros/marshallers/ProductMarshaller.groovy
+++ src/main/groovy/com/bestseguros/marshallers/ProductMarshaller.groovy
@@ -2,15 +2,15 @@ package com.bestseguros.marshallers
 
 import com.bestseguros.Product
 import grails.converters.JSON
-import org.codehaus.groovy.grails.web.converters.marshaller.ObjectMarshaller
+import org.grails.web.converters.marshaller.ObjectMarshaller
 
-class ProductMarshaller implements ObjectMarshaller{
+class ProductMarshaller implements ObjectMarshaller<JSON>{
 
   boolean supports(Object object){
     object instanceof Product
   }
   
-  void marshalObject(object,json){
+  void marshalObject(Object object,JSON json){
     def product = object as Product
     def productValues = [id:product.uuid,
                          name:product.name]
