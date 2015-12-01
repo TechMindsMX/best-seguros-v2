9691c9a4cdd2742594d3759470dfbfa982bc1164
egjimenezg <egjimenezg@gmail.com>
2015-12-01 09:28:16 -0600

    coding #8 agregando rsvp para el manejo de las promesas

--- grails-app/assets/javascripts/application.js
+++ grails-app/assets/javascripts/application.js
@@ -1,3 +1,4 @@
 //= require third_party/jquery/dist/jquery.js
 //= require third_party/bootstrap/dist/js/bootstrap.js
 //= require third_party/handlebars/handlebars.js
+//= require third_party/rsvp.js/rsvp.js
