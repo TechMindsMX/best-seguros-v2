dc50024e4c6bfa4549d15d90234d857dda050986
egjimenezg <egjimenezg@gmail.com>
2015-12-01 16:08:40 -0600

    coding #8 agregando el modelo para obtener los planes

---
+++ grails-app/assets/javascripts/plan/plan.js
@@ -0,0 +1,28 @@
+var Plan = {
+  id:'',
+  name:'',
+  
+  create: function(data){
+    return $.extend({},this,data);
+  },
+  
+  list: function(params){
+    return new RSVP.Promise(function(resolve,reject){
+      $.ajax({
+        url:params.url,
+        type:'GET',
+        data:params.data,
+        datatype:'json',
+        contentType:'application/json; charset=utf-8'
+      })
+      .done(function(response){
+        console.log(response);
+        resolve(response);
+      })
+      .fail(function(response){
+        reject(response);
+      });
+    });
+  }
+
+}
