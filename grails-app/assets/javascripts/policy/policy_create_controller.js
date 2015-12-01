e3fdf798edd7a7797948acc949667b0bfd70211d
egjimenezg <egjimenezg@gmail.com>
2015-12-01 11:45:38 -0600

    coding #8 agregando controllador js

--- grails-app/assets/javascripts/policy/policy_create_controller.js
+++ grails-app/assets/javascripts/policy/policy_create_controller.js
@@ -1,11 +1,21 @@
+//= require product/product.js
+
 var PolicyCreateController = (function(){
 
   var settings = {
     insuranceSelector:'select[name=insurance]'
   };
 
+  var producListSuccess = function(data){
+
+  };
+  
+  var failure = function(data){
+    console.log("Error " + data);
+  }
+
   var updateProducts = function(){
-    console.log("Updating products");
+    Product.list({url:'/products',data:{id:$(this).val()}}).then(producListSuccess,failure);
   };
 
   var bindEvents = function(){
