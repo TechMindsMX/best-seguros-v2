7043f64a2fb3865eedc6adf9b32be7dc2237b314
Gamaliel Jimenez García <egjimenezg@gmail.com>
2015-12-03 12:44:29 -0600

    coding #12 agregando marshaller para los rangos de edad por beneficiario

---
+++ src/main/groovy/com/bestseguros/marshallers/RangeAgeByBeneficiaryMarshaller.groovy
@@ -0,0 +1,23 @@
+package com.bestseguros.marshallers
+
+import com.bestseguros.RangeAgeByBeneficiary
+import grails.converters.JSON
+import org.grails.web.converters.marshaller.ObjectMarshaller
+
+class RangeAgeByBeneficiaryMarshaller implements ObjectMarshaller<JSON>{
+
+  boolean supports(Object object){
+    object instanceof RangeAgeByBeneficiary
+  }
+
+  void marshalObject(Object object,JSON json){
+    def rangeAgeByBeneficiary = object as RangeAgeByBeneficiary
+    def rangeValues = [insured:rangeAgeByBeneficiary.insuredType.value,
+                       minAge:rangeAgeByBeneficiary.minAge,
+                       maxAge:rangeAgeByBeneficiary.maxAge,
+                       renewal:rangeAgeByBeneficiary.renewal]
+
+    json.convertAnother(rangeValues)
+  }
+
+}
