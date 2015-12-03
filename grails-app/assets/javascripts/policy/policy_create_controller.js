840a2b0647807becf05cdd56e02ce6a7f68ffa66
Gamaliel Jimenez García <egjimenezg@gmail.com>
2015-12-03 16:12:21 -0600

    coding #12 agregando accion para obtener la información de los planes de la póliza

--- grails-app/assets/javascripts/policy/policy_create_controller.js
+++ grails-app/assets/javascripts/policy/policy_create_controller.js
@@ -1,38 +1,39 @@
 //= require product/product.js
 //= require plan/plan.js
-//= require product/product_select_view.js
 //= require plan/plan_select_view.js
+//= require plan/plan_info_view.js
 
 var PolicyCreateController = (function(){
 
   var settings = {
-    insuranceSelector:'select[name=insurance]',
     productSelector:'select[name=product]',
-    productsListDiv:'div.form-group.products'
-  };
-
-  var producListSuccess = function(data){
-    ProductSelectView.render(data); 
+    planSelect:'select[name=plan]',
+    productsListDiv:'div.productPlans',
   };
 
   var planListSuccess = function(data){
     PlanSelectView.render(data);
   }
-  
+
+  var planInfoSuccess = function(data){
+    PlanInfoView.render(data);
+  }
+
   var failure = function(data){
     console.log("Error " + data);
   }
-
-  var updateProducts = function(){
-    Product.list({url:'/products',data:{id:$(this).val()}}).then(producListSuccess,failure);
-  };
 
   var updatePlans = function(){
     Plan.list({url:'/plans',data:{id:$(this).val()}}).then(planListSuccess,failure);
   };
 
+  var showPlanInfo = function(){
+    Plan.get({url:'/plans/'+$(this).val()}).then(planInfoSuccess,failure);
+  }
+
   var bindEvents = function(){
     $(settings.productSelector).on("change",updatePlans);
+    $(settings.productsListDiv).on("change",settings.planSelect,showPlanInfo);
   };
 
   var start = function(){
