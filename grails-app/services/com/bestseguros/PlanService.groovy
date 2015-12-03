f384fd3e3a6cf57fe820c90d6097e11fa1c1ced7
Gamaliel Jimenez García <egjimenezg@gmail.com>
2015-12-02 18:00:12 -0600

    coding #12 agregando método en el servicio para obtener la información a mostrar

--- grails-app/services/com/bestseguros/PlanService.groovy
+++ grails-app/services/com/bestseguros/PlanService.groovy
@@ -4,9 +4,21 @@ import grails.transaction.Transactional
 
 @Transactional
 class PlanService {
-  
+
   def findPlansForProduct(String uuid){
     Product.findByUuid(uuid).plans
   }
 
+  def getPlanDetail(Plan plan){
+    def product = plan.product
+    def insuredSumsByCoveragePerInsured = [:]
+
+    plan.insureds.each{ insured ->
+      insuredSumsByCoveragePerInsured[insured] = plan.insuredSumsByCoveragePerInsured.findAll{ it.insured == insured }
+    }
+
+    [insuredSumsByCoveragePerInsured:insuredSumsByCoveragePerInsured,
+     rangeAgesByBeneficiary:product.rangesAgeByBeneficiary,
+     insuranceCostsPerInsured:product.insuranceCostsPerInsured]
+  }
 }
