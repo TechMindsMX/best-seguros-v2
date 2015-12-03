c98817e18f4e1de69fe2c0609bbb6bd4df1eb02b
Gamaliel Jimenez García <egjimenezg@gmail.com>
2015-12-02 18:01:54 -0600

    coding #12 agregando marshaller de coberturas

---
+++ src/main/groovy/com/bestseguros/marshallers/CoverageMarshaller.groovy
@@ -0,0 +1,20 @@
+package com.bestseguros.marshallers
+
+import grails.converters.JSON
+import org.grails.web.converters.marshaller.ObjectMarshaller
+import com.bestseguros.Coverage
+
+class Coverage implements ObjectMarshaller<JSON>{
+
+  boolean supports(Object object){
+    object instanceof Coverage
+  }
+
+  void marshalObject(Object object,JSON json){
+    def coverage = object as Coverage
+    def coverageValues = [name:coverage.name]
+
+    json.converAnother(coverageValues)
+  }
+
+}
