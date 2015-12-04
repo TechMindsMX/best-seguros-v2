d678d61de8ca67dcef3466d0576bbf3f707bb475
Gamaliel Jimenez García <egjimenezg@gmail.com>
2015-12-04 10:11:00 -0600

    coding #11 agregando nueva prueba para registrar un producto de la póliza

--- src/test/groovy/com/bestseguros/PolicyServiceSpec.groovy
+++ src/test/groovy/com/bestseguros/PolicyServiceSpec.groovy
@@ -6,7 +6,7 @@ import spock.lang.Specification
 import java.lang.Void as Should
 
 @TestFor(PolicyService)
-@Mock([Policy])
+@Mock([Policy,Plan,Product])
 class PolicyServiceSpec extends Specification {
 
   Should "create a new Policy"(){
@@ -14,6 +14,23 @@ class PolicyServiceSpec extends Specification {
       def policy = service.createPolicy()
     then:
       policy.id
+  }
+
+  Should "save the product and the plan of the policy"(){
+    given:"the product"
+      def plan = new Plan(name:"Titular",
+                         insureds:[InsuredType.PRINCIPAL]).save(validate:false)
+
+      def product = new Product(name:"Renta diaria por Hospitalización")
+      product.addToPlans(plan)
+      product.save(validate:false)
+    and:"the policy"
+      def policy = new Policy(policyStatus:PolicyStatus.CREATED).save()
+    when:
+      def policyWithProduct = service.addProductAndPlanToPolicy(policy.id,[product:product.id,plan:plan.id])
+    then:
+      policyWithProduct.product.id
+      policyWithProduct.plan.id
   }
 
 }
