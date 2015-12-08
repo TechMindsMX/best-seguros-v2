ae2548cc6824b4c969ec5b799f89c41c2cc2e527
egjimenezg <egjimenezg@gmail.com>
2015-12-08 15:49:39 -0600

    coding #13 agregando prueba para el método que registra un asegurado

---
+++ src/test/groovy/com/bestseguros/InsuredServiceSpec.groovy
@@ -0,0 +1,38 @@
+package com.bestseguros
+
+import grails.test.mixin.TestFor
+import grails.test.mixin.Mock
+import spock.lang.Specification
+import java.lang.Void as Should
+
+@TestFor(InsuredService)
+@Mock([Insured,Policy])
+class InsuredServiceSpec extends Specification {
+
+  Should "save an insured for a policy"(){
+    given:"the policy"
+      def policy = new Policy(policyStatus:PolicyStatus.CREATED).save(validate:false)
+    and:"an insured"
+      def insured = new Insured(name:"Gamaliel",
+                                lastName:"Jiménez",
+                                motherLastName:"Garcia",
+                                phone:"5548109233",
+                                rfc:"JIGE930831",
+                                email:"egjimenezg@gmail.com",
+                                address:"Bellas Artes 232",
+                                country:"México",
+                                state:"Distrito Federal",
+                                town:"Some Town",
+                                colony:"Colony",
+                                cp:"57730",
+                                city:"México City",
+                                insuredType:InsuredType.PRINCIPAL)
+      insured.birthDate = new Date()
+    when:
+      service.addInsuredForPolicy(insured,policy)
+    then:
+      policy.insureds.size() == 1
+      policy.insureds.first().id 
+  }
+ 
+}
