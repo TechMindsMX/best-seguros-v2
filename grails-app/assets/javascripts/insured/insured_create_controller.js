fb85e711688432c6c6b08599046a4d018dd87d20
Gamaliel Jimenez García <egjimenezg@gmail.com>
2015-12-09 16:52:32 -0600

    coding #16 agregando controller para mostrar los datepickers

---
+++ grails-app/assets/javascripts/insured/insured_create_controller.js
@@ -0,0 +1,31 @@
+//= require third_party/pickadate/lib/picker.js
+//= require third_party/pickadate/lib/picker.date.js
+//= require third_party/pickadate/lib/translations/es_ES.js
+
+var InsuredCreateController = (function(){
+
+  var settings = {
+    datePicker:'.datePicker'
+  };
+
+  var start = function(){
+    $(settings.datePicker).pickadate({
+      selectYears: true,
+      selectMonths: true,
+      selectYears: 110,
+      format:'dd/mm/yyyy',
+      max: true,
+      today: false,
+      clear: false,
+      close: 'Cerrar'});
+  };
+
+  return{
+    start:start
+  };
+
+}());
+
+jQuery(function($){
+  InsuredCreateController.start();
+});
