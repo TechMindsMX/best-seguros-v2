c3567ab478863ef51131245c8ccdfcf3e69a6c54
egjimenezg <egjimenezg@gmail.com>
2015-12-15 11:36:41 -0600

    coding #10 agregando tamaño máximo en los formularios

--- grails-app/domain/com/bestseguros/Insured.groovy
+++ grails-app/domain/com/bestseguros/Insured.groovy
@@ -27,6 +27,7 @@ class Insured{
     lastName blank:false
     motherLastName blank:false
     rfc blank:false,nullable:true,size:10..15,matches:/[A-Z]{4}([0-9]{2})(1[0-2]|0[1-9])([0-3][0-9])([A-Z0-9]{3})$/
+    phone bank:false,matches:/[0-9]+/
     email blank:false, email:true
     address blank:false
     country blank:false
