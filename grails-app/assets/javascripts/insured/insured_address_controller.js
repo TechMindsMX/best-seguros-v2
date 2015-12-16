111bc472f8d974bdbb3a49848d60decb80df5a62
Gamaliel Jimenez García <egjimenezg@gmail.com>
2015-12-16 11:57:49 -0600

    coding #21 arreglando la obtención de direcciones para formularios independientes

--- grails-app/assets/javascripts/insured/insured_address_controller.js
+++ grails-app/assets/javascripts/insured/insured_address_controller.js
@@ -1,40 +1,40 @@
 //= require address/address.js
 
 var InsuredAddressController = (function(){
-  
+
   var settings = {
-    colony:'#colony',
-    town:'#town',
-    city:'#city',
-    state:'#state',
-    cp:'#cp',
+    cp:'.cp',
     url:'#sepomexUrl'
   };
 
+  var currentForm = null;
+
   var showAddressInfo = function(){
-    Address.get({url:$(settings.url).val()+$(settings.cp).val()}).then(success,failure);
+    if($(this).val() !== ''){
+      currentForm = $(this).parents("form");
+      Address.get({url:$(settings.url).val()+$(this).val()}).then(success,failure);
+    }
   }
 
   var success = function(data){
-    console.log(data);
-    $(settings.colony).val(data.dAsenta[0]); 
-    $(settings.town).val(data.dMnpio);
-    $(settings.state).val(data.dEstado);
-    $(settings.city).val(data.dCiudad); 
+    currentForm.find(".colony").val(data.dAsenta[0]);
+    currentForm.find(".town").val(data.dMnpio);
+    currentForm.find(".state").val(data.dEstado);
+    currentForm.find(".city").val(data.dCiudad);
   };
 
   var failure = function(data){
-
-  }; 
+    console.log(data);
+  };
 
   var bindEvents = function(){
     $(settings.cp).on("blur",showAddressInfo);
-  }; 
+  };
 
   var start = function(){
-    bindEvents(); 
+    bindEvents();
   };
-  
+
   return{
     start:start
   };
