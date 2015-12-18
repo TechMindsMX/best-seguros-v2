02ceb78e1c127688af3fd4f9d1415509d2506dcb
Gamaliel Jimenez García <egjimenezg@gmail.com>
2015-12-18 17:12:40 -0600

    coding #24 agregando archivos necesarios en application.js y application.css

--- grails-app/assets/javascripts/application.js
+++ grails-app/assets/javascripts/application.js
@@ -3,11 +3,13 @@
 //= require third_party/handlebars/handlebars.js
 //= require third_party/rsvp.js/rsvp.js
 //= require handlebars/helper_controller.js
+//= require home/home_controller.js
 
 var App = (function(){
 
   var start = function(){
     HelperController.start();
+    HomeController.start();
   };
 
   return{
