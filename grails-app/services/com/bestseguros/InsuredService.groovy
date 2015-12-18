e7e2bf85ed15a5164eb566cdee48138734659e4e
Gamaliel Jimenez García <egjimenezg@gmail.com>
2015-12-18 12:34:56 -0600

    coding #23 agregando método en el servicio para guardar un contratante y un asegurado

--- grails-app/services/com/bestseguros/InsuredService.groovy
+++ grails-app/services/com/bestseguros/InsuredService.groovy
@@ -1,14 +1,28 @@
 package com.bestseguros
 
 import grails.transaction.Transactional
+import org.grails.core.DefaultGrailsDomainClass
 
 @Transactional
 class InsuredService {
 
   def addInsuredForPolicy(Insured insured,Policy policy){
-    policy.addToInsureds(insured) 
+    policy.addToInsureds(insured)
     policy.save()
     policy
+  }
+
+  def addContratingPartyAndPrincipalForPolicy(Insured insured,Policy policy){
+    def domainClass = new DefaultGrailsDomainClass(Insured.class)
+    def fields = domainClass.persistentProperties*.name
+    def principal = new Insured()
+
+    fields.each{ field ->
+      principal[field] = insured[field]
+    }
+
+    addInsuredForPolicy(principal,policy)
+    addInsuredForPolicy(insured,policy)
   }
 
 }
