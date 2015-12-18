54ae5e4c7567ba46a9f968aa71140ccad20e3cd1
Gamaliel Jimenez García <egjimenezg@gmail.com>
2015-12-18 16:21:43 -0600

    coding #23 verificando si el contratante también es titular en el controller

--- grails-app/controllers/com/bestseguros/InsuredController.groovy
+++ grails-app/controllers/com/bestseguros/InsuredController.groovy
@@ -11,14 +11,26 @@ class InsuredController {
     if(insured.hasErrors()){
       def insureds = policyService.findSavedAndUnsavedInsuredsForPolicy(policy)
       def insuredTypes = policy?.plan?.insureds*.insured.unique() ?: []
-      insureds.unsavedInsureds[insured.insuredType].remove(0)
-      insureds.unsavedInsureds[insured.insuredType].add(0,insured)
-      render view:"/policy/edit",model:[policy:policy,insuredTypes:insuredTypes.sort(),insureds:insureds]
+      def model = [policy:policy,insuredTypes:insuredTypes.sort()]
+
+      if(insured.insuredType != InsuredType.CONTRACTING_PARTY){
+        insureds.unsavedInsureds[insured.insuredType].remove(0)
+        insureds.unsavedInsureds[insured.insuredType].add(0,insured)
+      }
+      else{
+        model.contractingParty = policyService.getContractingParty(policy) ?: insured
+        model.principal = params.boolean("principal")
+      }
+
+      model.insureds = insureds
+
+      render view:"/policy/edit",model:model
       return
     }
 
-    if(params.isPrincipal)
+    if(params.boolean("principal")){
       insuredService.addContratingPartyAndPrincipalForPolicy(insured,policy)
+    }
     else
       insuredService.addInsuredForPolicy(insured,policy)
 
