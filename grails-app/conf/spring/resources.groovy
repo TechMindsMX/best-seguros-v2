fb62e6b1f459567a4f9d668c86314cae1a40d654
Gamaliel Jimenez García <egjimenezg@gmail.com>
2015-12-23 17:59:38 -0600

    coding #28 agregando route para el procesamiento del archivo

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,9 +1,13 @@
 // Place your Spring DSL code here
+import com.bestseguros.routes.MyRouter
+
 beans = {
 
   localeResolver(org.springframework.web.servlet.i18n.SessionLocaleResolver) {
     defaultLocale = new Locale("es","MX")
     java.util.Locale.setDefault(defaultLocale)
   }
+
+  myRouter(MyRouter)
 
 }
