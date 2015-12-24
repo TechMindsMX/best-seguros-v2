cf71d57a45daae5fafd49748bca8b1493ad06561
Gamaliel Jimenez García <egjimenezg@gmail.com>
2015-12-24 11:44:53 -0600

    coding #28 renombrando el route para el procesamiento del archivo

--- src/main/groovy/com/bestseguros/routes/FileRoute.groovy
+++ src/main/groovy/com/bestseguros/routes/FileRoute.groovy
@@ -3,14 +3,14 @@ package com.bestseguros.routes
 import org.apache.camel.builder.RouteBuilder
 import org.apache.camel.Exchange
 
-class MyRouter extends RouteBuilder{
+class FileRoute extends RouteBuilder{
 
   @Override
   void configure() throws Exception{
     from("file:///Users/makingdevs/Documents/SoftwareDevelopment/MakingDevs/filesToProcess?delete=true")
     //.filter { exchange -> exchange.in.headers.CamelFileName.endsWith("xls") }
     .filter { exchange -> exchange.in.headers.CamelFileName.endsWith("csv") }
-    .split(body(String.class).tokenize(","))
+    //.split(body(String.class).tokenize(","))
     //.aggregate(header("CamelFileName"), new StringAggregationStrategy()).completionTimeout(3000)
     .to("log:groovymail?showAll=true&multiline=true")
     .to("file:///Users/makingdevs/Documents/SoftwareDevelopment/MakingDevs/exported")
