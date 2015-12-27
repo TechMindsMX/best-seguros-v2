a6c559b3212aedf6222c5738d1e51559c4005244
egjimenezg <egjimenezg@gmail.com>
2015-12-26 21:16:37 -0600

    coding #28 actualizando clase FileRoute

--- src/main/groovy/com/bestseguros/routes/FileRoute.groovy
+++ src/main/groovy/com/bestseguros/routes/FileRoute.groovy
@@ -2,18 +2,34 @@ package com.bestseguros.routes
 
 import org.apache.camel.builder.RouteBuilder
 import org.apache.camel.Exchange
+import org.apache.camel.CamelContext
+import org.apache.camel.spring.SpringCamelContext
+import org.springframework.context.annotation.Configuration
+import org.apache.camel.spring.javaconfig.SingleRouteCamelConfiguration
 
-class FileRoute extends RouteBuilder{
+@Configuration
+class FileRoute extends SingleRouteCamelConfiguration{
+
+  def grailsApplication 
 
   @Override
-  void configure() throws Exception{
-    from("file:///Users/makingdevs/Documents/SoftwareDevelopment/MakingDevs/filesToProcess?delete=true")
-    //.filter { exchange -> exchange.in.headers.CamelFileName.endsWith("xls") }
-    .filter { exchange -> exchange.in.headers.CamelFileName.endsWith("csv") }
-    //.split(body(String.class).tokenize(","))
-    //.aggregate(header("CamelFileName"), new StringAggregationStrategy()).completionTimeout(3000)
-    .to("log:groovymail?showAll=true&multiline=true")
-    .to("file:///Users/makingdevs/Documents/SoftwareDevelopment/MakingDevs/exported")
+  protected CamelContext createCamelContext(){
+    new SpringCamelContext(getApplicationContext())
+  }
+
+  @Override
+  public RouteBuilder route(){
+    return new RouteBuilder(){
+      void configure() throws Exception{
+        from("file:///Users/egjimenezg/Documents/SoftwareDevelopment/MakingDevs/TechMindsMX/filesToProcess?delete=true")
+        //.filter { exchange -> exchange.in.headers.CamelFileName.endsWith("xls") }
+        .filter { exchange -> exchange.in.headers.CamelFileName.endsWith("csv") }
+        //.split(body(String.class).tokenize(","))
+        //.aggregate(header("CamelFileName"), new StringAggregationStrategy()).completionTimeout(3000)
+        .to("log:groovymail?showAll=true&multiline=true")
+        .to("file:///Users/egjimenezg/Documents/SoftwareDevelopment/MakingDevs/TechMindsMX/exported")
+      }
+    }
   }
 
 }
