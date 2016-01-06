c29b858069f29997f710b19dca7c8e26a1c4006f
Gamaliel Jimenez García <egjimenezg@gmail.com>
2016-01-06 10:56:35 -0600

    coding #30 mostrando la lista de productos para realizar la búsqueda

--- grails-app/assets/javascripts/policy/policy_list_controller.js
+++ grails-app/assets/javascripts/policy/policy_list_controller.js
@@ -1,23 +1,41 @@
 //= require insurance/insurance.js
+//= require product/product.js
 //= require policy/policy_search_view.js
+//= require product/product_list_view.js
 
 var PolicyListController = (function(){
   var settings = {
-    insuranceListUrl:'#insuranceLink'
+    insuranceListUrl:'#insuranceLink',
+    productListUrl:'#productLink',
+    insuranceSelect:'select[name=insurance]',
+    productListDiv:'.productListDiv'
   };
-  
+
   var success = function(data){
-    PolicySearchView.render(data); 
+    PolicySearchView.render(data);
   };
-  
+
+  var showProducts = function(){
+    Product.list({url:$(settings.productListUrl).val(),data:{id:$(this).val()}}).then(productListSuccess,failure);
+  }
+
+  var productListSuccess = function(data){
+    ProductListView.render(data,settings.productListDiv);
+  };
+
   var failure = function(data){
-    console.log("Error! " + data);
+    console.log("Error: " + data);
   };
+
+  var bindEvents = function(){
+    $("#applicationDiv").on("change",settings.insuranceSelect,showProducts);
+  }
 
   var start = function(){
     Insurance.list({url:$(settings.insuranceListUrl).val()}).then(success,failure);
+    bindEvents();
   };
-  
+
   return{
     start:start
   };
