11ba87846b0c7e340a55447b0b2f3708dda82e65
Gamaliel Jimenez García <egjimenezg@gmail.com>
2016-01-06 15:24:42 -0600

    coding #30 agregando controller para realizar las búsquedas

---
+++ grails-app/assets/javascripts/policy/policy_search_controller.js
@@ -0,0 +1,35 @@
+//= require policy/policy.js
+
+var PolicySearchController = (function(){
+
+  var settings = {
+    searchButton:'#searchButton',
+    productSelect:'select[name=products]'
+  };
+
+  var success = function(data){
+    console.log(data);
+  };
+
+  var failure = function(data){
+    console.log("Error "+data);
+  };
+
+  var findPolicies = function(event){
+    event.preventDefault();
+    Policy.list({url:$(this).parents('form').attr('action'),data:{product:$(settings.productSelect).val()}}).then(success,failure);
+  };
+
+  var bindEvents = function(){
+    $("#applicationDiv").on("click",settings.searchButton,findPolicies);
+  };
+
+  var start = function(){
+    bindEvents();
+  };
+
+  return{
+    start:start
+  };
+
+}());
