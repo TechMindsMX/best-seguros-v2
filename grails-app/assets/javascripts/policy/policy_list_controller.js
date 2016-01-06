83127c04fd00dea2b2f26c2bb79ccb2dc4a415b6
Gamaliel Jimenez García <egjimenezg@gmail.com>
2016-01-06 11:14:34 -0600

    coding #30 mostrando y ocultando el botón cuando el producto se selecciona

--- grails-app/assets/javascripts/policy/policy_list_controller.js
+++ grails-app/assets/javascripts/policy/policy_list_controller.js
@@ -8,16 +8,14 @@ var PolicyListController = (function(){
     insuranceListUrl:'#insuranceLink',
     productListUrl:'#productLink',
     insuranceSelect:'select[name=insurance]',
-    productListDiv:'.productListDiv'
+    productListDiv:'.productListDiv',
+    productSelect:'select[name=products]',
+    searchButton:'#searchButton'
   };
 
   var success = function(data){
     PolicySearchView.render(data);
   };
-
-  var showProducts = function(){
-    Product.list({url:$(settings.productListUrl).val(),data:{id:$(this).val()}}).then(productListSuccess,failure);
-  }
 
   var productListSuccess = function(data){
     ProductListView.render(data,settings.productListDiv);
@@ -27,8 +25,21 @@ var PolicyListController = (function(){
     console.log("Error: " + data);
   };
 
+  var showProducts = function(){
+    Product.list({url:$(settings.productListUrl).val(),data:{id:$(this).val()}}).then(productListSuccess,failure);
+  };
+
+  var showButton = function(){
+    if($(this).val() != '')
+      $(searchButton).removeClass('hidden');
+    else
+      $(searchButton).addClass('hidden');
+
+  };
+
   var bindEvents = function(){
     $("#applicationDiv").on("change",settings.insuranceSelect,showProducts);
+    $("#applicationDiv").on("change",settings.productSelect,showButton);
   }
 
   var start = function(){
