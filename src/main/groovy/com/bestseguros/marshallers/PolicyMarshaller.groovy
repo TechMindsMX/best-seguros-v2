cc1912bb1ae19293f6795a7d9436105b422f05d0
Gamaliel Jimenez García <egjimenezg@gmail.com>
2016-01-06 15:10:31 -0600

    coding #30 agregado marshaller para póliza

---
+++ src/main/groovy/com/bestseguros/marshallers/PolicyMarshaller.groovy
@@ -0,0 +1,22 @@
+package com.bestseguros.marshallers
+
+import grails.converters.JSON
+import org.grails.web.converters.marshaller.ObjectMarshaller
+import com.bestseguros.Policy
+
+class PolicyMarshaller implements ObjectMarshaller<JSON>{
+
+  boolean supports(Object object){
+    object instanceof Policy
+  }
+
+  void marshalObject(Object object,JSON json){
+    def policy = object as Policy
+    def policyValues = [product:policy.product,
+                        plan:policy.plan,
+                        status:policy.policyStatus.value]
+
+    json.convertAnother(policyValues)
+  }
+
+}
