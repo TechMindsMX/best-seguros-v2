29c0dcb7445fa895ef64ba0124136a6f636ccfe9
Gamaliel Jimenez García <egjimenezg@gmail.com>
2016-01-06 11:34:17 -0600

    coding #30 agregando método en el servico para realizar la búsqueda de las pólizas

--- grails-app/services/com/bestseguros/PolicyService.groovy
+++ grails-app/services/com/bestseguros/PolicyService.groovy
@@ -91,4 +91,10 @@ class PolicyService {
     policy.insureds.find{ it.insuredType == InsuredType.CONTRACTING_PARTY }
   }
 
+  def findProductPolicies(Long productId){
+    def policies = Policy.findAll{
+      product.id == productId
+    }
+  }
+
 }
