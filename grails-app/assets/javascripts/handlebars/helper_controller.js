2e63d5b7b4fc8c742441b39722316f7610d2e26c
Gamaliel Jimenez García <egjimenezg@gmail.com>
2016-01-07 13:03:06 -0600

    coding #30 agregando estilo a los estatus de las pólizas

--- grails-app/assets/javascripts/handlebars/helper_controller.js
+++ grails-app/assets/javascripts/handlebars/helper_controller.js
@@ -1,8 +1,28 @@
 var HelperController = (function(){
 
+  var policyStatusLabelClass = {
+    creada:'blue',
+    finalizada:'green'
+  };
+
   var initHelpers = function(){
     Handlebars.registerHelper('showProperty',function(obj,field,options){
       return obj[field][options.hash.property];
+    });
+
+    Handlebars.registerHelper('labelType',function(object){
+      var status = Handlebars.escapeExpression(object);
+
+      return new Handlebars.SafeString(
+        policyStatusLabelClass[status.toLowerCase()]
+      );
+    });
+
+    Handlebars.registerHelper('showEditButton',function(conditional,options){
+      if(conditional== 'Creada')
+        return options.fn(this);
+      else
+        return options.inverse(this);
     });
   };
 
