d2a2cb4b676e7e03903d22e1bc27b1e0742ae36d
Gamaliel Jimenez García <egjimenezg@gmail.com>
2016-01-11 12:49:03 -0600

    coding #31 verificando si es tarjeta o cuenta bancaria

--- grails-app/assets/javascripts/policy/policy_payment_method_controller.js
+++ grails-app/assets/javascripts/policy/policy_payment_method_controller.js
@@ -1,15 +1,27 @@
+//= require bank/bank_list_controller.js
+
 var PolicyPaymentMethodController = (function(){
 
   var settings = {
     paymentType:'#paymentType'
   };
 
+  var paymentTypes = {
+    cards:['DEBIT_CARD','CREDIT_CARD'],
+    bankAccounts:['CHECK','REFERENCED_DEPOSIT']
+  }
+
   var start = function(){
     bindEvents();
   };
 
   var showBanksOrProviders = function(){
+    if(paymentTypes.cards.indexOf($(this).val()) >= 0){
 
+    }
+    else if(paymentTypes.bankAccounts.indexOf($(this).val()) >= 0){
+      BankListController.start();
+    }
   };
 
   var bindEvents = function(){
