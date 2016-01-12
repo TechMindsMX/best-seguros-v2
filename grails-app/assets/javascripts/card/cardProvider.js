3a4cbb64af495645f3f641b630010ce96cfb17a5
egjimenezg <egjimenezg@gmail.com>
2016-01-12 16:30:09 -0600

    coding #31 agregando modelo js cardProvider

---
+++ grails-app/assets/javascripts/card/cardProvider.js
@@ -0,0 +1,47 @@
+var CardProvider = {
+  key:'',
+  value:'',
+ 
+  create:function(data){
+    return $.extend({},this,data);
+  },
+
+  deserialize:function(data){
+    var _self = CardProvider.create({});
+    
+    Object.keys(_self).forEach(function(key){
+      if(typeof _self[key] !== 'function'){
+        _self[key] = data.cardProvider[key];
+      }
+    });
+
+    return _self;
+  }, 
+
+  list: function(params){
+    return new RSVP.Promise(function(resolve,reject){
+      $.ajax({
+        url:params.url,
+        type:'GET',
+        data:params.data,
+        datatype:'json',
+        contentType:'application/json; charset=utf-8'
+      }) 
+      .done(function(response){
+        console.log(response); 
+
+        var model = {cardProviders:[]}; 
+
+        response.forEach(function(item){
+          model.cardProviders.push(CardProvider.deserialize({cardProvider:item}));
+        });
+
+        resolve(model);
+      })
+      .fail(function(response){
+        reject(response);
+      });
+    });  
+  }
+
+};
