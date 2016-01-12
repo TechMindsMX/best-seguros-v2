306ca49e151958ef42c6a91019dbd554a6b578d6
egjimenezg <egjimenezg@gmail.com>
2016-01-12 12:58:14 -0600

    coding #32 enviando mensaje al actor

--- grails-app/services/com/bestseguros/MessageService.groovy
+++ grails-app/services/com/bestseguros/MessageService.groovy
@@ -1,21 +1,20 @@
 package com.bestseguros
 
 import grails.transaction.Transactional
-import org.springframework.jms.annotation.JmsListener
 import org.apache.camel.*
-import com.bestseguros.actor.MailSender
+import com.bestseguros.actor.MailSenderActor
 
 class MessageService {
 
-  def mailSender
+  def mailSenderActor
 
   def sendPolicyMail(Exchange exchange){
     def policy = exchange.getIn().getBody(Policy.class)
 
-    if(!mailSender.isActive())
-      mailSender.start()
+    if(!mailSenderActor.isActive())
+      mailSenderActor.start()
 
-    mailSender.send policy.id
+    mailSenderActor.send policy.id
   }
 
 }
