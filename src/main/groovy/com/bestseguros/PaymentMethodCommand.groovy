c399792b46aa8879ee5006fa9bec2e357feecd4a
Gamaliel Jimenez García <egjimenezg@gmail.com>
2016-01-13 17:32:14 -0600

    coding #31 agregando clase payment method command

---
+++ src/main/groovy/com/bestseguros/PaymentMethodCommand.groovy
@@ -0,0 +1,10 @@
+package com.bestseguros
+
+class PaymentMethodCommand{
+  String cardNumber
+  String accountNumber
+  PaymentType paymentType
+  Periodicity periodicity
+  CardProvider cardProvider
+  Bank bank
+}
