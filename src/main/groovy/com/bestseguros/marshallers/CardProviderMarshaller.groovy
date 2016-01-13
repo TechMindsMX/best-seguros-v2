8156bd3ddfb6a9c78c99f751d2347adbfd161631
Gamaliel Jimenez García <egjimenezg@gmail.com>
2016-01-13 15:33:14 -0600

    coding #31 corrigiendo marshaller

--- src/main/groovy/com/bestseguros/marshallers/CardProviderMarshaller.groovy
+++ src/main/groovy/com/bestseguros/marshallers/CardProviderMarshaller.groovy
@@ -1,15 +1,20 @@
 package com.bestseguros.marshallers
 
 import grails.converters.JSON
+import org.grails.web.converters.marshaller.ObjectMarshaller
 import com.bestseguros.CardProvider
 
-class CardProviderMarshaller{
+class CardProviderMarshaller implements ObjectMarshaller<JSON>{
 
-  void register(){
-    JSON.registerObjectMarshaller(CardProvider){ CardProvider cardProvider ->
-      [id:cardProvider.key,
-       value:cardProvider.value]
-    }
+  boolean supports(Object object){
+    object instanceof CardProvider
+  }
+
+  void marshalObject(Object object,JSON json){
+    def cardProvider = object as CardProvider
+    def cardProviderValues = [id:cardProvider.key,
+                              value:cardProvider.value]
+    json.convertAnother(cardProviderValues)
   }
 
 }
