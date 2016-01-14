920b8dc93ddb5c4b1eb9f41d7e9e735c5f966baf
Gamaliel Jimenez García <egjimenezg@gmail.com>
2016-01-14 14:47:17 -0600

    coding #31 validando que la póliza no tenga un método de pago ya

--- grails-app/services/com/bestseguros/PaymentMethodService.groovy
+++ grails-app/services/com/bestseguros/PaymentMethodService.groovy
@@ -5,18 +5,25 @@ import grails.transaction.Transactional
 @Transactional
 class PaymentMethodService {
 
+  def paymentService
+
   def addPaymentMethodToPolicy(Policy policy,PaymentMethodCommand paymentMethodCommand){
-    def paymentTypes = [[PaymentType.CREDIT_CARD,PaymentType.DEBIT_CARD]:new Card(cardNumber:paymentMethodCommand.cardNumber,
+    if(!policy.payment){
+      def paymentTypes = [[PaymentType.CREDIT_CARD,PaymentType.DEBIT_CARD]:new Card(cardNumber:paymentMethodCommand.cardNumber,
                                                                                   cardProvider:paymentMethodCommand.cardProvider),
                         [PaymentType.CHECK,PaymentType.REFERENCED_DEPOSIT]:new BankAccount(bank:paymentMethodCommand.bank,
                                                                                            accountNumber:paymentMethodCommand.accountNumber)]
-    def paymentMethod = paymentTypes.find{ it.key.contains(paymentMethodCommand.paymentType) }?.value
-    paymentMethod.paymentType = paymentMethod.paymentType
-    paymentMethod.periodicity = paymentMethod.periodicity
-    def payment = new Payment(paymentMethodRef:paymentMethod.id,
-                              type:paymentMethod.class.simpleName)
-    policy.payment = payment
-    payment
+
+      def paymentMethod = paymentTypes.find{ it.key.contains(paymentMethodCommand.paymentType) }?.value
+      paymentMethod.paymentType = paymentMethodCommand.paymentType
+      paymentMethod.periodicity = paymentMethodCommand.periodicity
+      paymentMethod.save()
+
+      def payment = paymentService.createPaymentForInstance(paymentMethod)
+      policy.payment = payment
+      policy.save()
+    }
+    policy
   }
 
 }
