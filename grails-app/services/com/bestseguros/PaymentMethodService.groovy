762a33ab157e782267d41f3a6329f7bee8f49383
Gamaliel Jimenez García <egjimenezg@gmail.com>
2016-01-14 12:04:56 -0600

    coding #31 agregando el pago a la póliza

--- grails-app/services/com/bestseguros/PaymentMethodService.groovy
+++ grails-app/services/com/bestseguros/PaymentMethodService.groovy
@@ -5,11 +5,18 @@ import grails.transaction.Transactional
 @Transactional
 class PaymentMethodService {
 
-  def addPaymentMethodToPolicy(Policy policy,PaymentMethodCommand paymentMethod){
-    def paymentTypes = [[PaymentType.CREDIT_CARD,PaymentType.DEBIT_CARD]:new Card(),
-                        [PaymentType.CHECK,PaymentType.REFERENCED_DEPOSIT]:new BankAccount()]
-
-
+  def addPaymentMethodToPolicy(Policy policy,PaymentMethodCommand paymentMethodCommand){
+    def paymentTypes = [[PaymentType.CREDIT_CARD,PaymentType.DEBIT_CARD]:new Card(cardNumber:paymentMethodCommand.cardNumber,
+                                                                                  cardProvider:paymentMethodCommand.cardProvider),
+                        [PaymentType.CHECK,PaymentType.REFERENCED_DEPOSIT]:new BankAccount(bank:paymentMethodCommand.bank,
+                                                                                           accountNumber:paymentMethodCommand.accountNumber)]
+    def paymentMethod = paymentTypes.find{ it.key.contains(paymentMethodCommand.paymentType) }?.value
+    paymentMethod.paymentType = paymentMethod.paymentType
+    paymentMethod.periodicity = paymentMethod.periodicity
+    def payment = new Payment(paymentMethodRef:paymentMethod.id,
+                              type:paymentMethod.class.simpleName)
+    policy.payment = payment
+    payment
   }
 
 }
