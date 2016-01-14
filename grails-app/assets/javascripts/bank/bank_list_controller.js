cdd9c0e73edc3604c56054ddf3e1465c32bdfa71
Gamaliel Jimenez García <egjimenezg@gmail.com>
2016-01-14 12:46:03 -0600

    coding #31 agregando validaciones para cuando se trate de una cuenta bancaria

--- grails-app/assets/javascripts/bank/bank_list_controller.js
+++ grails-app/assets/javascripts/bank/bank_list_controller.js
@@ -3,8 +3,21 @@
 
 var BankListController = (function(){
 
+  var settings = {
+    paymentMethodForm:'form[name=paymentMethodForm]',
+    bankSelector:'select[name=bank]',
+    accountNumber:'input[name=accountNumber]'
+  };
+
+  var addValidations = function(){
+    $(settings.paymentMethodForm).validate();
+    $(settings.bankSelector).rules("add","required");
+    $(settings.accountNumber).rules("add","required");
+  };
+
   var successBankList = function(data){
     BankListView.render(data);
+    addValidations();
   };
 
   var failureBankList = function(data){
