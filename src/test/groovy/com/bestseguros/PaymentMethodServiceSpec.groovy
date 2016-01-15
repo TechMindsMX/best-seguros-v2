ac0314735efff29c30634833950c23a60601130a
Gamaliel Jimenez García <egjimenezg@gmail.com>
2016-01-15 10:22:19 -0600

    coding #37 agregando colaborador a la prueba

--- src/test/groovy/com/bestseguros/PaymentMethodServiceSpec.groovy
+++ src/test/groovy/com/bestseguros/PaymentMethodServiceSpec.groovy
@@ -5,7 +5,7 @@ import grails.test.mixin.Mock
 import spock.lang.Specification
 import java.lang.Void as Should
 
-@Mock([Policy])
+@Mock([Policy,Card,BankAccount])
 @TestFor(PaymentMethodService)
 class PaymentMethodServiceSpec extends Specification {
 
@@ -15,9 +15,12 @@ class PaymentMethodServiceSpec extends Specification {
 
     and:"the payment method"
       def paymentMethod = _paymentMethod
+      def paymentService = Mock(PaymentService)
+      service.paymentService = paymentService
     when:
       service.addPaymentMethodToPolicy(policy,paymentMethod)
     then:
+      1 * paymentService.createPaymentForInstance(_) >> new Payment(paymentMethodRef:1,type:_paymentType)
       policy.payment.type == _paymentType
     where:
       _paymentMethod                                                                                                              || _paymentType
