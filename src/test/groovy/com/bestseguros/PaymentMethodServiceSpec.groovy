82592757573d01bdc8b1faa41ce5e5e7c4663115
Gamaliel Jimenez García <egjimenezg@gmail.com>
2016-01-19 10:44:27 -0600

    coding #36 agregando prueba para el servicio que asigna el método de pago a la póliza

--- src/test/groovy/com/bestseguros/PaymentMethodServiceSpec.groovy
+++ src/test/groovy/com/bestseguros/PaymentMethodServiceSpec.groovy
@@ -5,14 +5,13 @@ import grails.test.mixin.Mock
 import spock.lang.Specification
 import java.lang.Void as Should
 
-@Mock([Policy,Card,BankAccount,Payment])
+@Mock([Policy,Card,BankAccount,Payment,Bank])
 @TestFor(PaymentMethodService)
 class PaymentMethodServiceSpec extends Specification {
 
   Should "add the payment method to the policy"(){
     given: "the policy"
       def policy = new Policy(policyStatys:PolicyStatus.CREATED)
-
     and:"the payment method"
       def paymentMethod = _paymentMethod
       def paymentService = Mock(PaymentService)
@@ -26,6 +25,28 @@ class PaymentMethodServiceSpec extends Specification {
       _paymentMethod                                                                                                              || _paymentType
       new PaymentMethodCommand(cardNumber:"012345678912345",paymentType:PaymentType.CREDIT_CARD,periodicity:Periodicity.MONTHLY)  || Card.class.simpleName
       new PaymentMethodCommand(accountNumber:"012345678910",paymentType:PaymentType.CHECK,periodicity:Periodicity.QUARTERLY)      || BankAccount.class.simpleName
+  }
+
+  Should "create the payment for the policy"(){
+    given:"the policy"
+      def policy = new Policy(policyStatus:PolicyStatus.CREATED)
+      def bank = new Bank(name:"NAFIN")
+      bank.save()
+    and:"the payment service"
+      def paymentService = Mock(PaymentService)
+      def paymentMethod = _paymentMethod
+      service.paymentService = paymentService
+    when:
+      def policyWithPayment = service.createPaymentForPolicy(policy,paymentMethod)
+    then:
+      1 * paymentService.createPaymentForInstance(_) >> { pm -> def payment = new Payment(paymentMethodRef:1,type:pm.class.simpleName)
+                                                                payment.save()
+                                                                payment
+                                                         }
+      policyWithPayment.payment.id
+    where:
+      _paymentMethod << [new BankAccount(accountNumber:"123456789012345",paymentType:PaymentType.CHECK,periodicity:Periodicity.ANNUAL,bank:new Bank(name:"NAFIN").save()),
+                         new Card(cardNumber:"123456789012345",cardProvider:CardProvider.VISA,paymentType:PaymentType.CREDIT_CARD,periodicity:Periodicity.MONTHLY)]
   }
 
   Should "check if the policy has a payment method"(){
